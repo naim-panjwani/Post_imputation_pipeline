5baa4e6b4e7b126e3050fdc51c0fe01413154b36
Naim Panjwani <naim.panjwani@gmail.com>
2019-07-05 16:26:10 -0400

    Step 5 fully debugged

--- 05-VCF_Reformating.py
+++ 05-VCF_Reformating.py
@@ -11,6 +11,8 @@ import gzip
 import sys
 from tqdm import tqdm
 import argparse
+import re
+from datetime import datetime
 
 ###### Helper functions ######
 def getNumHeaderLines(vcf_filename, num_lines_to_check = 1000):
@@ -116,59 +118,87 @@ def writeHeader(infile, outfilename):
         with gzip.open(outfilename, 'wb') as f_out:
             nextline = f_in.readline().decode('utf-8')
             while nextline[0:1] == "#":
-                f_out.write(bytes(nextline, 'UTF-8'))
+                f_out.write(bytes(nextline, 'utf-8'))
                 nextline = f_in.readline().decode('utf-8')
 
 
 ##############################
 ## MAIN
 ##############################
-parser = argparse.ArgumentParser(description="Tool to format a raw VCF to match the format of BEAGLE (v5) output VCF")
-parser.add_argument('vcf_filename', help='The VCF filename that needs reformatting')
-parser.add_argument('chunksize', help='Size this number according to available RAM; recommended value is 1000 rows')
-parser.add_argument('output_filename', help='Output filename')
-args = parser.parse_args()
-vcf_filename = args.vcf_filename
-chunksize = int(args.chunksize)
-outfilename = args.output_filename
-
-print('Checking number of VCF header lines')
-numHeaderLines = getNumHeaderLines(vcf_filename)
-print('Reading VCF in chunks of ' + str(chunksize) + ' lines')
-vcfchunks = pd.read_csv(vcf_filename, compression='gzip', sep='\t', skiprows=numHeaderLines, header=0, chunksize=chunksize)
-#vcfchunk = pd.read_csv("23-SNPs_to_add_back_normalized_chunk.vcf.gz", compression='gzip', sep='\t', skiprows=numHeaderLines, header=0)
-
-writeHeader(vcf_filename, outfilename)
-for vcfchunk in vcfchunks:
-    filtercol = list(vcfchunk['FILTER'])
-    infocol = list(vcfchunk['INFO'])
-    formatcol = list(vcfchunk['FORMAT'])
-    gt_df = vcfchunk.iloc[:, 9:]
-    filtercol = [x.replace('.', 'PASS') for x in filtercol]
-    formatcol = [x.replace('GT','GT:DS:GP') for x in formatcol]
-    for row in np.arange(vcfchunk.shape[0]):
-        if vcfchunk.iloc[row,3] == '.':
-            sys.exit("Missing REF alleles detected! Exiting")
-        if vcfchunk.iloc[row,4] == '.':
-            infocol[row] = 'DR2=1.00;AF=0'
-            gt_df.iloc[row,:] = [str(x) + ':0:1,0,0' for x in gt_df.iloc[row,:]]
-            continue
-        GTlist = list(gt_df.iloc[row,:])
-        alt_alleles = vcfchunk.iloc[row,4].split(',')
-        allele_counter = 0
-        AF = []
-        DSlist = []
-        GPlist = []
-        for alt_allele in alt_alleles:
-            allele_counter += 1            
-            AF.append(getAF(GTlist, allele = allele_counter))
-            DSlist.append(getDosage(GTlist, allele = allele_counter))
-            GPlist.append(getGP(GTlist, allele = allele_counter))
-        AFstring = ','.join([f'{x:.4f}' for x in AF])
-        infocol[row] = f'DR2=1.00;AF={AFstring}'
-        gt_df.iloc[row,:] = appendToGT(GTlist, DSlist, GPlist)
-    vcfchunk['INFO'] = infocol
-    vcfchunk['FORMAT'] = formatcol
-    vcfchunk.iloc[:, 9:] = gt_df
-    vcfchunk.to_csv(outfilename, sep='\t', index=False, mode='a', compression='gzip', encoding='utf-8', header=False)
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(description="Tool to format a raw VCF to match the format of BEAGLE (v5) output VCF")
+    parser.add_argument('vcf_filename', help='The VCF filename that needs reformatting')
+    #parser.add_argument('chunksize', help='Size this number according to available RAM; recommended value is 10000 rows')
+    parser.add_argument('output_filename', help='Output filename')
+    args = parser.parse_args()
+    vcf_filename = args.vcf_filename
+    chunksize = 100
+    #chunksize = int(args.chunksize)
+    outfilename = args.output_filename
+    logfilename = outfilename.replace('.gz','').replace('.vcf','')+'.log'
+    
+    old_stdout = sys.stdout
+    log_file = open(logfilename, "w")
+    sys.stdout = log_file
+    
+    #vcf_filename = '23-SNPs_to_add_back_normalized_chunk.vcf.gz'
+    #outfilename = '24-SNPs_to_add_back_reformatted_chunk.vcf.gz'
+    #chunksize = 100
+    
+    print(datetime.now().strftime('%c'))
+    print('VCF file: ' + str(vcf_filename))
+    print('Output file: ' + str(outfilename))
+    print('Chunk size: ' + str(chunksize))
+    
+    print('Checking number of VCF header lines')
+    numHeaderLines = getNumHeaderLines(vcf_filename)
+    print('Reading VCF in chunks of ' + str(chunksize) + ' lines')
+    vcfchunks = pd.read_csv(vcf_filename, compression='gzip', sep='\t', skiprows=numHeaderLines, header=0, chunksize=chunksize)
+    #vcfchunk = pd.read_csv("23-SNPs_to_add_back_normalized_chunk.vcf.gz", compression='gzip', sep='\t', skiprows=numHeaderLines, header=0)
+    
+    print('Writing header')
+    writeHeader(vcf_filename, outfilename)
+    print('Reformatting and saving in chunks')
+    for vcfchunk in vcfchunks:
+        filtercol = list(vcfchunk['FILTER'])
+        infocol = list(vcfchunk['INFO'])
+        formatcol = list(vcfchunk['FORMAT'])
+        gt_df = vcfchunk.iloc[:, 9:]
+        filtercol = [x.replace('.', 'PASS') for x in filtercol]
+        formatcol = [x.replace('GT','GT:DS:GP') for x in formatcol]
+        for row in np.arange(vcfchunk.shape[0]):
+            if vcfchunk.iloc[row,3] == '.':
+                sys.exit("Missing REF alleles detected! Exiting")
+            if vcfchunk.iloc[row,4] == '.':
+                infocol[row] = 'DR2=1.00;AF=0'
+                gt_df.iloc[row,:] = [str(x) + ':0:1,0,0' for x in gt_df.iloc[row,:]]
+                continue
+            GTlist = list(gt_df.iloc[row,:])
+            alt_alleles = vcfchunk.iloc[row,4].split(',')
+            allele_counter = 0
+            AF = []
+            DSlist = []
+            GPlist = []
+            for alt_allele in alt_alleles:
+                allele_counter += 1            
+                AF.append(getAF(GTlist, allele = allele_counter))
+                DSlist.append(getDosage(GTlist, allele = allele_counter))
+                GPlist.append(getGP(GTlist, allele = allele_counter))
+            AFstring = ','.join([f'{x:.4f}' for x in AF])
+            infocol[row] = f'DR2=1.00;AF={AFstring}'
+            gt_df.iloc[row,:] = appendToGT(GTlist, DSlist, GPlist)
+        vcfchunk['FILTER'] = filtercol
+        vcfchunk['INFO'] = infocol
+        vcfchunk['FORMAT'] = formatcol
+        vcfchunk.iloc[:, 9:] = gt_df
+        #vcfchunk.to_csv(outfilename, sep='\t', mode='a', index=False, header=False, compression='gzip')
+        vcfchunk_str = vcfchunk.to_string(index=False, header=False)
+        vcfchunk_str = re.sub(r"\ +", '\t', vcfchunk_str)
+        vcfchunkb = bytes(vcfchunk_str, 'utf-8')
+        with gzip.open(outfilename, 'a') as f_out:
+            f_out.write(vcfchunkb)
+    
+    print(datetime.now().strftime('%c'))
+    sys.stdout = old_stdout
+    log_file.close()
 
